bff4d4604cd5a1fe9ac611b5f14810ccf069859b
maggie mu <maggiemu_matrix@hotmail.com>
2015-02-24 21:48:48 +1100

    add grunt task for uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,26 +28,68 @@ module.exports = function(grunt) {
                 browsers: ['PhantomJS']
             }
         },
+        copy:{
+            main: {
+                files: [
+                    {src: 'app/index-dev.html', dest: 'dist/index.html'},
+                    {expand: true, cwd: 'app', src: 'phones/**', dest: 'dist/'},
+                    {expand: true, cwd: 'app', src: 'partials/**', dest: 'dist/'},
+                    {expand: true, cwd: 'app', src: 'img/**', dest: 'dist/'}
+                ]
+            }
+        },
         watch: {
             files: ['<%= jshint.files %>', 'app/**/*.js', 'test/**/*.js'],
             tasks: ['karma:unit']
         },
-        open : {
-            coverage: {
-                path: 'coverage/Chrome 40.0.2214 (Mac OS X 10.9.4)/index.html',
-                app: 'Google Chrome'
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 8
+            },
+            source: {
+                files: [{
+                    src: [
+                        'dist/js/**/*.js',
+                        'dist/css/**/*.css'
+                    ]
+                }]
             }
-        }
+        },
+        useminPrepare: {
+            html: 'app/index-dev.html',
+            options: {
+                flow: {
+                    steps: {
+                        js: ['uglifyjs'],
+                        css: ['cssmin']
+                    },
+                    post: {}
+                }
+            }
+        },
+        usemin:{
+            html:['dist/index.html']
+        },
+        clean: ['build', 'dist']
     });
 
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-filerev');
 
     grunt.registerTask('test', ['karma:unit']);
 
-    grunt.registerTask('default', ['jshint', 'karma:unit']);
-    grunt.registerTask('deploy', ['karma:continuous']);
+    grunt.registerTask('default', ['karma:continuous', 'clean', 'copy',  'useminPrepare', 'uglify', 'cssmin', 'filerev', 'usemin']);
+    grunt.registerTask('deploy', ['karma:continuous', 'useminPrepare']);
 };
